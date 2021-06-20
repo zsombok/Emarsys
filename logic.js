4faefbe16fe7d130979a9688551b75077dd101c8
zsombok <zsombokd@gmail.com>
2021-06-20 23:05:51 +0200

    Add files via upload

---
+++ logic.js
@@ -0,0 +1,76 @@
+let submitDateTime
+const workStartTime = 9
+const workEndTime = 17
+const workingHoursDuration = 8
+
+function calculateDueDate(dateAndTimeOfSubmission, turnaroundTime = 0) {
+  if (!isDate(dateAndTimeOfSubmission)) { return "Date of submission is not valid!" }
+  if (!Number.isInteger(turnaroundTime)) { return "Turnaround time is not valid! Please provide duration in hours." }
+
+  let DueDateOfResolve = new Date(dateAndTimeOfSubmission)
+
+  if (DueDateOfResolve.getHours() < workStartTime || DueDateOfResolve.getHours() >= workEndTime) { return "Please report your problem during working hours (9AM to 5PM)!" }
+
+  const { days, hours } = convertTurnaroundTimeToDaysAndHours(turnaroundTime)
+  addDaysToDate(DueDateOfResolve, days)
+  addHoursToTime(DueDateOfResolve, hours)
+  if (isOutOfWorkingHours(DueDateOfResolve)) {
+    addDaysToDate(DueDateOfResolve, 1)
+    const excessTime = DueDateOfResolve.getHours() % workEndTime
+    DueDateOfResolve.setHours((workStartTime + excessTime))
+  }
+
+  skipWeekend(DueDateOfResolve)
+
+  return `Issue is due to be resolved by ${DueDateOfResolve.toString()}`
+}
+
+function addDaysToDate(date, days) {
+  date.setDate(date.getDate() + days)
+}
+
+function addHoursToTime(date, hours) {
+  date.setHours(date.getHours() + hours)
+}
+
+function convertTurnaroundTimeToDaysAndHours(time) {
+  const days = Math.floor(time / workingHoursDuration)
+  const hours = time % workingHoursDuration
+  return { days, hours }
+}
+
+function isOutOfWorkingHours(date) {
+  if (date.getHours() >= workEndTime) { return true }
+  return false
+}
+
+function skipWeekend(date) {
+  if (date.getDay() === 0 || date.getDay() === 6) {
+    date.setDate(date.getDate() + 1)
+    skipWeekend(date)
+  }
+}
+
+// https://jsfiddle.net/remarkablemark/7wegs1ao/
+function isDate(value) {
+  switch (typeof value) {
+    case 'number':
+      return true;
+    case 'string':
+      return !isNaN(Date.parse(value));
+    case 'object':
+      if (value instanceof Date) {
+        return !isNaN(value.getTime());
+      }
+    default:
+      return false;
+  }
+}
+
+// NAIVE TESTING
+
+submitDateTime = '2021 06 16 9:24:00'
+
+for (let i = 0; i < 40; i++) {
+  console.log(calculateDueDate(submitDateTime, 3*i))
+}
